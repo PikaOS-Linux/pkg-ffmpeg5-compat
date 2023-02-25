c9a04f394e144bb9bca0e5b3f651a12062d3d7d7
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-25 21:26:47 +0300

    fix rules #9

---
+++ debian/99-ffmpeg5.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/opt/ffmpeg5/usr/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ffmpeg5/usr/lib/x86_64-linux-gnu:/opt/ffmpeg5/usr/lib/i386-linux-gnu
+export PKG_CONFIG_PATH=$ PKG_CONFIG_PATH:/opt/ffmpeg5/usr/lib/x86_64-linux-gnu/pkgconfig:/opt/ffmpeg5/usr/lib/i386-linux-gnu/pkgconfig
