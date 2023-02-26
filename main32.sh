fbd6856d3a97ecb6c7592b006b155a295ebafa3b
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-26 16:42:02 +0300

    add main32.sh

---
+++ main32.sh
@@ -0,0 +1,24 @@
+# Add dependent repositories
+sudo dpkg --add-architecture i386
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+
+# Clone Upstream
+git clone https://github.com/FFmpeg/FFmpeg -b release/5.1
+cp -rvf ./debian ./FFmpeg
+mv ./FFmpeg ./ffmpeg
+cd ./ffmpeg
+
+# Get build deps
+apt-get build-dep ./ -y -a i386
+
+# Build package
+LOGNAME=root dh_make --createorig -y -l -p ffmpeg_5.1.3
+dpkg-buildpackage -a i386
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
