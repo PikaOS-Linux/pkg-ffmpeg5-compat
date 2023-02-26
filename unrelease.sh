811421fb0551f957df4325e42607de473e45c6cc
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-26 19:48:01 +0300

    add unrelease

---
+++ unrelease.sh
@@ -0,0 +1,16 @@
+# Pull down existing ppa repo db files etc
+rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
+
+# Remove our existing package from the repo
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% ffmpeg5*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavcodec59*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavdevice59*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavfilter8*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavformat59*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavutil57*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libpostproc56*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswresample4*)'
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswscale6*)'
+
+# Add the new package to the repo
+reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/*.deb
