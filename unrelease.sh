196202ae6f8657866a25ffda6a7bdf84b14fbbf6
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-04-30 00:58:43 +0300

    update to lunar

--- unrelease.sh
+++ unrelease.sh
@@ -3,15 +3,15 @@ mkdir -p ./output/repo
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% ffmpeg5*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavcodec59*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavdevice59*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavfilter8*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavformat59*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavutil57*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libpostproc56*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswresample4*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswscale6*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% ffmpeg5*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavcodec59*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavdevice59*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavfilter8*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavformat59*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavutil57*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libpostproc56*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libswresample4*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libswscale6*)'
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
